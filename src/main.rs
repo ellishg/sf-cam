dac4da27c320e79fe153711b696f86cb56b8f8fd
Ellis Hoag <ellis.sparky.hoag@gmail.com>
2024-11-12 23:07:30 -0800

    Serve latest image

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use esp_idf_svc::{
 };
 use log::info;
 use sf_cam::esp_camera::Camera;
+use std::sync::{Arc, Mutex};
 use std::time::Duration;
 
 #[derive(Debug)]
@@ -71,10 +72,23 @@ fn main() -> Result<()> {
 
     let mut server = http::server::EspHttpServer::new(&http::server::Configuration::default())?;
 
-    server.fn_handler::<anyhow::Error, _>("/", http::Method::Get, |request| {
+    let last_framebuffer = Arc::new(Mutex::new(Vec::new()));
+
+    let last_framebuffer_clone = last_framebuffer.clone();
+    server.fn_handler::<anyhow::Error, _>("/", http::Method::Get, move |request| {
         info!("GET request /");
-        let mut response = request.into_ok_response()?;
-        response.write_all("ok".as_bytes())?;
+        let data = last_framebuffer_clone.lock().unwrap().to_vec();
+        if data.len() > 0 {
+            let headers = [
+                ("Content-Type", "image/jpeg"),
+                ("Content-Length", &data.len().to_string()),
+            ];
+            let mut response = request.into_response(200, Some("OK"), &headers)?;
+            response.write_all(data.as_slice())?;
+        } else {
+            let mut response = request.into_ok_response()?;
+            response.write_all("No image yet.".as_bytes())?;
+        }
         Ok(())
     })?;
 
@@ -102,21 +116,6 @@ fn main() -> Result<()> {
         esp_idf_sys::camera::camera_grab_mode_t_CAMERA_GRAB_WHEN_EMPTY,
     )?;
 
-    server.fn_handler::<anyhow::Error, _>("/index.html", http::Method::Get, move |request| {
-        info!("GET request /index.html");
-
-        // let headers = [
-        //     ("Content-Type", "image/jpeg"),
-        //     ("Content-Length", &data.len().to_string()),
-        // ];
-        // let mut response = request.into_response(200, Some("OK"), &headers).unwrap();
-        // response.write_all(data)?;
-        let mut response = request.into_ok_response()?;
-        response.write_all("TODO: Report interesting info here.".as_bytes())?;
-
-        Ok(())
-    })?;
-
     // Flash to know that we have connected to wifi and the server is setup.
     camera_flash.set_high()?;
     delay.delay_ms(100);
@@ -126,7 +125,13 @@ fn main() -> Result<()> {
         .timelapse_period
         .parse::<humantime::Duration>()?
         .into();
-    generate_timelapse(camera, timelapse_period, CONFIG.picture_count, delay)?;
+    generate_timelapse(
+        camera,
+        timelapse_period,
+        CONFIG.picture_count,
+        last_framebuffer,
+        delay,
+    )?;
 
     // Keep wifi and the server running beyond when main() returns (forever)
     // Do not call this if you ever want to stop or access them later.
@@ -143,17 +148,18 @@ fn generate_timelapse(
     camera: Camera,
     timelapse_period: Duration,
     picture_count: u32,
+    last_framebuffer: Arc<Mutex<Vec<u8>>>,
     delay: Delay,
 ) -> Result<()> {
     assert!(picture_count > 0);
 
     let delay_time = timelapse_period / picture_count;
     for _ in 0..picture_count - 1 {
-        capture_picture(&camera)?;
+        capture_picture(&camera, last_framebuffer.clone())?;
         delay.delay_ms(delay_time.as_millis().try_into()?);
     }
     // We don't want a delay after the last picture.
-    capture_picture(&camera)?;
+    capture_picture(&camera, last_framebuffer.clone())?;
 
     // TODO: load all pictures and generate video timelapse
     // TODO: save to file
@@ -161,10 +167,11 @@ fn generate_timelapse(
     Ok(())
 }
 
-fn capture_picture(camera: &Camera) -> Result<()> {
-    let _framebuffer = camera
+fn capture_picture(camera: &Camera, last_framebuffer: Arc<Mutex<Vec<u8>>>) -> Result<()> {
+    let framebuffer = camera
         .get_framebuffer()
         .expect("unable to capture framebuffer");
+    *last_framebuffer.lock().unwrap() = framebuffer.data().to_vec();
     // TODO: Save to file
     Ok(())
 }
